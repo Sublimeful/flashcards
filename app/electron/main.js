66281bc7569a18d65ebe96f3509762d2a830fded
unknown <Qiangwu2000@gmail.com>
2022-05-14 13:15:16 -0500

    Polised welcome screen

--- app/electron/main.js
+++ app/electron/main.js
@@ -5,6 +5,8 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 800,
+    minHeight: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
--- app/src/pages/welcome/welcome.js
+++ app/src/pages/welcome/welcome.js
@@ -1,7 +1,4 @@
-const create_flashcard_btn = document.getElementById("create_flashcard_btn")
-
-
-  console.log("Clicked")
+const create_flashcard_btn = document.getElementById("create-flashcard-btn")
 create_flashcard_btn.onclick = function() {
   console.log("Clicked")
   load_flashcards()
