593fab699ea42cbf799655d4d091efc2ce4941db
unknown <Qiangwu2000@gmail.com>
2022-05-14 11:50:40 -0500

    Flashcards page added

--- app/src/index.js
+++ app/src/index.js
@@ -1,16 +1,40 @@
 const path = require("path")
-const fs = require("fs");
+const fs = require("fs").promises;
 
 let window_el = document.querySelector("#window")
 let pages = path.resolve(path.join(__dirname, "pages"))
 let welcome_page = path.resolve(path.join(pages, "welcome"))
+let flashcards_page = path.resolve(path.join(pages, "flashcards"))
 
-function load_welcome() {
-  fs.readFile(path.resolve(path.join(welcome_page, "welcome.html")), "utf8", (err, data) => {
-    window_el.innerHTML = ''
-    console.log(data)
-    window_el.innerHTML = data
-  })
+function executeScriptElements(containerElement) {
+  const scriptElements = containerElement.querySelectorAll("script");
+
+  Array.from(scriptElements).forEach((scriptElement) => {
+    const clonedElement = document.createElement("script");
+
+    Array.from(scriptElement.attributes).forEach((attribute) => {
+      clonedElement.setAttribute(attribute.name, attribute.value);
+    });
+    
+    clonedElement.text = scriptElement.text;
+
+    scriptElement.parentNode.replaceChild(clonedElement, scriptElement);
+  });
+}
+
+async function load_page(page_path) {
+  const html = await fs.readFile(page_path, "utf8")
+  window_el.innerHTML = ''
+  window_el.insertAdjacentHTML("beforeend", html);
+  executeScriptElements(window_el);
+}
+
+async function load_welcome() {
+  await load_page(path.resolve(path.join(welcome_page, "welcome.html")))
+}
+
+async function load_flashcards() {
+  await load_page(path.resolve(path.join(flashcards_page, "flashcards.html")))
 }
 
 load_welcome()
---
+++ app/src/pages/welcome/welcome.js
@@ -0,0 +1,8 @@
+const create_flashcard_btn = document.getElementById("create_flashcard_btn")
+
+
+  console.log("Clicked")
+create_flashcard_btn.onclick = function() {
+  console.log("Clicked")
+  load_flashcards()
+}
